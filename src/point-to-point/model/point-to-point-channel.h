4f6d524794fc0c8d92faed828dffa91df79436c9
weidehui <40263901+weidehui@users.noreply.github.com>
2018-08-16 23:56:01 +0800

    Update point-to-point-channel.h

--- src/point-to-point/model/point-to-point-channel.h
+++ src/point-to-point/model/point-to-point-channel.h
@@ -43,17 +43,25 @@ class Packet;
  * There are two "wires" in the channel.  The first device connected gets the
  * [0] wire to transmit on.  The second device gets the [1] wire.  There is a
  * state (IDLE, TRANSMITTING) associated with each wire.
+ *
+ * \see Attach
+ * \see TransmitStart
  */
 class PointToPointChannel : public Channel 
 {
 public:
+  /**
+   * \brief Get the TypeId
+   *
+   * \return The TypeId for this class
+   */
   static TypeId GetTypeId (void);
 
   /**
    * \brief Create a PointToPointChannel
    *
-   * By default, you get a channel that
-   * has zero transmission delay.
+   * By default, you get a channel that has an "infinitely" fast 
+   * transmission speed and zero delay.
    */
   PointToPointChannel ();
 
@@ -70,7 +78,7 @@ public:
    * \param txTime Transmit time to apply
    * \returns true if successful (currently always true)
    */
-  virtual bool TransmitStart (Ptr<Packet> p, Ptr<PointToPointNetDevice> src, Time txTime);
+  virtual bool TransmitStart (Ptr<const Packet> p, Ptr<PointToPointNetDevice> src, Time txTime);
 
   /**
    * \brief Get number of devices on this channel
@@ -78,34 +86,36 @@ public:
    */
   virtual uint32_t GetNDevices (void) const;
 
-  /*
+  /**
    * \brief Get PointToPointNetDevice corresponding to index i on this channel
    * \param i Index number of the device requested
    * \returns Ptr to PointToPointNetDevice requested
    */
   Ptr<PointToPointNetDevice> GetPointToPointDevice (uint32_t i) const;
 
-  /*
+  /**
    * \brief Get NetDevice corresponding to index i on this channel
    * \param i Index number of the device requested
    * \returns Ptr to NetDevice requested
    */
   virtual Ptr<NetDevice> GetDevice (uint32_t i) const;
+  void dretimer();
+
 
 protected:
-  /*
+  /**
    * \brief Get the delay associated with this channel
    * \returns Time delay
    */
   Time GetDelay (void) const;
 
-  /*
+  /**
    * \brief Check to make sure the link is initialized
    * \returns true if initialized, asserts otherwise
    */
   bool IsInitialized (void) const;
 
-  /*
+  /**
    * \brief Get the net-device source 
    * \param i the link requested
    * \returns Ptr to PointToPointNetDevice source for the 
@@ -113,7 +123,7 @@ protected:
    */
   Ptr<PointToPointNetDevice> GetSource (uint32_t i) const;
 
-  /*
+  /**
    * \brief Get the net-device destination
    * \param i the link requested
    * \returns Ptr to PointToPointNetDevice destination for 
@@ -121,13 +131,31 @@ protected:
    */
   Ptr<PointToPointNetDevice> GetDestination (uint32_t i) const;
 
+ // void dretimer();
+
+  /**
+   * TracedCallback signature for packet transmission animation events.
+   *
+   * \param [in] packet The packet being transmitted.
+   * \param [in] txDevice the TransmitTing NetDevice.
+   * \param [in] rxDevice the Receiving NetDevice.
+   * \param [in] duration The amount of time to transmit the packet.
+   * \param [in] lastBitTime Last bit receive time (relative to now)
+   * \deprecated The non-const \c Ptr<NetDevice> argument is deprecated
+   * and will be changed to \c Ptr<const NetDevice> in a future release.
+   */
+  typedef void (* TxRxAnimationCallback)
+    (Ptr<const Packet> packet,
+     Ptr<NetDevice> txDevice, Ptr<NetDevice> rxDevice,
+     Time duration, Time lastBitTime);
+                    
 private:
-  // Each point to point link has exactly two net devices
+  /** Each point to point link has exactly two net devices. */
   static const int N_DEVICES = 2;
 
-  Time          m_delay;
-  int32_t       m_nDevices;
-
+  Time          m_delay;    //!< Propagation delay
+  int32_t       m_nDevices; //!< Devices of this channel
+  float         CECount;
   /**
    * The trace source for the packet transmission animation events that the 
    * device can fire.
@@ -135,33 +163,49 @@ private:
    * net device, receiving net device, transmission time and 
    * packet receipt time.
    *
-   * @see class CallBackTraceSource
+   * \see class CallBackTraceSource
+   * \deprecated The non-const \c Ptr<NetDevice> argument is deprecated
+   * and will be changed to \c Ptr<const NetDevice> in a future release.
    */
-  TracedCallback<Ptr<const Packet>, // Packet being transmitted
-                 Ptr<NetDevice>,    // Transmitting NetDevice
-                 Ptr<NetDevice>,    // Receiving NetDevice
-                 Time,              // Amount of time to transmit the pkt
-                 Time               // Last bit receive time (relative to now)
+  TracedCallback<Ptr<const Packet>,     // Packet being transmitted
+                 Ptr<NetDevice>,  // Transmitting NetDevice
+                 Ptr<NetDevice>,  // Receiving NetDevice
+                 Time,                  // Amount of time to transmit the pkt
+                 Time                   // Last bit receive time (relative to now)
                  > m_txrxPointToPoint;
 
+  /** \brief Wire states
+   *
+   */
   enum WireState
   {
+    /** Initializing state */
     INITIALIZING,
+    /** Idle state (no transmission from NetDevice) */
     IDLE,
+    /** Transmitting state (data being transmitted from NetDevice. */
     TRANSMITTING,
+    /** Propagating state (data is being propagated in the channel. */
     PROPAGATING
   };
 
+  /**
+   * \brief Wire model for the PointToPointChannel
+   */
   class Link
   {
 public:
+    /** \brief Create the link, it will be in INITIALIZING state
+     *
+     */
     Link() : m_state (INITIALIZING), m_src (0), m_dst (0) {}
-    WireState                  m_state;
-    Ptr<PointToPointNetDevice> m_src;
-    Ptr<PointToPointNetDevice> m_dst;
+
+    WireState                  m_state; //!< State of the link
+    Ptr<PointToPointNetDevice> m_src;   //!< First NetDevice
+    Ptr<PointToPointNetDevice> m_dst;   //!< Second NetDevice
   };
 
-  Link    m_link[N_DEVICES];
+  Link    m_link[N_DEVICES]; //!< Link model
 };
 
 } // namespace ns3
